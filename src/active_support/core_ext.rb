d3f7008a6ce727cd9a52dcb19076cfa0dc0dfa3e
Joseph Dayo <joseph.dayo@gmail.com>
2012-09-17 19:43:20 +0800

    moved active support files to the vendor folder, may increase
    load performance

--- src/active_support/core_ext.rb
+++
@@ -1,4 +0,0 @@
-Dir["#{File.dirname(__FILE__)}/core_ext/*.rb"].sort.each do |path|
-  next if File.basename(path, '.rb') == 'logger'
-  require "active_support/core_ext/#{File.basename(path, '.rb')}"
-end
