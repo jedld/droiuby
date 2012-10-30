2662465eb2e6b54af54bc769d03e6eb17a3d4cb3
Joseph Dayo <joseph.dayo@gmail.com>
2012-10-30 19:36:27 +0800

    CSS Selector parser WIP

--- src/com/droiuby/client/core/CssRules.java
+++ src/com/droiuby/client/core/CssRules.java
@@ -9,6 +9,102 @@ import android.view.View;
 
 import com.droiuby.client.core.builder.ViewBuilder;
 
+class Selector {
+	public static final int SELECT_ID = 0;
+	public static final int SELECT_CLASS = 1;
+	public static final int SELECT_TAG = 2;
+	public static final int COMPOUND_SELECTOR = 3;
+
+	String identifier;
+	int selectorType;
+
+	public int getSelectorType() {
+		return selectorType;
+	}
+
+	public void setSelectorType(int selectorType) {
+		this.selectorType = selectorType;
+	}
+
+	ArrayList<Selector> compound = new ArrayList<Selector>();
+	Selector child;
+
+	public Selector getChild() {
+		return child;
+	}
+
+	public void setChild(Selector child) {
+		this.child = child;
+	}
+
+	public void addSelector(Selector selector) {
+		compound.add(selector);
+	}
+
+	public ArrayList<Selector> getCompound() {
+		return compound;
+	}
+
+	public void setCompound(ArrayList<Selector> compound) {
+		this.compound = compound;
+	}
+
+	static final int STATE_TYPE_INITIAL = 0;
+	static final int STATE_TYPE_PARSE_IDENTIFIER = 0;
+	static final int SUB_STATE_TYPE_COMPOUND_SELECTOR = 1;
+
+	public static Selector parseSelector(String selector) {
+		Selector mainSelector = new Selector();
+		Selector current = mainSelector;
+		char selectorChars[] = selector.toCharArray();
+		int pos = 0;
+		int state = STATE_TYPE_INITIAL;
+		int sub_state = 0;
+		StringBuffer identifierNameBuffer = new StringBuffer();
+		do {
+			char c = selectorChars[pos];
+			if (state == STATE_TYPE_INITIAL) {
+				if (c == '#') {
+					current.setSelectorType(SELECT_ID);
+					state = STATE_TYPE_PARSE_IDENTIFIER;
+				} else if (c == '.') {
+					current.setSelectorType(SELECT_CLASS);
+					state = STATE_TYPE_PARSE_IDENTIFIER;
+				} else if (Character.isLetterOrDigit(c)) {
+					current.setSelectorType(SELECT_TAG);
+					state = STATE_TYPE_PARSE_IDENTIFIER;
+				}
+			} else
+				if (state == STATE_TYPE_PARSE_IDENTIFIER) {
+					
+					if (Character.isLetterOrDigit(c)) {
+						identifierNameBuffer.append(c);
+					} else
+						if ( (c == '.') || (c == '#') ) {
+							Selector subSelector = new Selector();
+							
+							subSelector.identifier = identifierNameBuffer.toString();
+							
+							identifierNameBuffer = new StringBuffer();
+							
+							if (sub_state!=SUB_STATE_TYPE_COMPOUND_SELECTOR) {
+								subSelector.setSelectorType(current.getSelectorType());
+								current.setSelectorType(COMPOUND_SELECTOR);
+							} else {
+								current.setSelectorType(SELECT_CLASS);
+							}
+							current.addSelector(subSelector);
+							sub_state = SUB_STATE_TYPE_COMPOUND_SELECTOR;
+							state = STATE_TYPE_PARSE_IDENTIFIER; 
+						}
+				}
+			pos++;
+		} while (pos < selectorChars.length);
+		return mainSelector;
+	}
+
+}
+
 public class CssRules {
 	ArrayList<CssRule> rules = new ArrayList<CssRule>();
 
@@ -19,21 +115,21 @@ public class CssRules {
 	public void setRules(ArrayList<CssRule> rules) {
 		this.rules = rules;
 	}
-	
+
 	public void addRule(CssRule rule) {
 		rules.add(rule);
 	}
-	
+
 	public void apply(ActivityBuilder activityBuilder, Context context) {
-		for(CssRule rule: rules) {
+		for (CssRule rule : rules) {
 			Object result = activityBuilder.findViewByName(rule.getSelector());
-			Log.d(this.getClass().toString(),"Apply CSS " + rule.getSelector());
+			Log.d(this.getClass().toString(), "Apply CSS " + rule.getSelector());
 			if (result instanceof View) {
 				setRuleToView(activityBuilder, context, rule, result);
-			} else
-			if (result instanceof ArrayList) {
-				Log.d(this.getClass().toString(),"Setting multiple view instances ...");
-				for(View view : (ArrayList<View>)result) {
+			} else if (result instanceof ArrayList) {
+				Log.d(this.getClass().toString(),
+						"Setting multiple view instances ...");
+				for (View view : (ArrayList<View>) result) {
 					setRuleToView(activityBuilder, context, rule, view);
 				}
 			}
@@ -42,11 +138,12 @@ public class CssRules {
 
 	private void setRuleToView(ActivityBuilder activityBuilder,
 			Context context, CssRule rule, Object result) {
-		ViewBuilder viewBuilder = ViewBuilder.getBuilderForView((View)result, context, activityBuilder);
-		HashMap <String,String> propertyMap = new HashMap <String,String>();
-		for(PropertyValue property : rule.getProperties()) {
+		ViewBuilder viewBuilder = ViewBuilder.getBuilderForView((View) result,
+				context, activityBuilder);
+		HashMap<String, String> propertyMap = new HashMap<String, String>();
+		for (PropertyValue property : rule.getProperties()) {
 			propertyMap.put(property.getProperty(), property.getValue());
 		}
-		viewBuilder.setParamsFromProperty((View)result, propertyMap);
+		viewBuilder.setParamsFromProperty((View) result, propertyMap);
 	}
 }
