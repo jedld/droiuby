80cefca1b4923d39f16120b481306a38670ccb24
Joseph Dayo <joseph.dayo@gmail.com>
2013-11-15 23:05:26 +0800

    Use lower platform version

--- assets/launcher/launcher_script.rb
+++ assets/launcher/launcher_script.rb
@@ -1,22 +1,31 @@
 class Main < Activity
   def on_create
-      V('#run').on(:click) do |view|
-          puts view.class.to_s
-          puts view.data('url')
-          app_url = V('#app_url')
-          launch app_url.text
-      end
+      if _P.contains_key :app_url
+        launch _P.get(:app_url)
+      else
       
-      V('#qrcode').on(:click) do |view|
-        integrator = Java::com.droiuby.client.utils.intents.IntentIntegrator.new(me)
-        integrator.initiateScan
-      end
-      
-    async.perform {
-      Java::com.droiuby.client.core.utils.Utils.getLocalIpAddress(_current_activity)
-    }.done { |result|
-      V('#ip_address').text = "#{result}:4000"
-    }.start
+          V('#run').on(:click) do |view|
+              puts view.class.to_sl
+              puts view.data('url')
+              app_url = V('#app_url')
+              
+              #store in prefs to be auto launched next time
+              _P.update_attributes!(app_url: app_url.text)
+              
+              launch app_url.text
+          end
+          
+          V('#qrcode').on(:click) do |view|
+            integrator = Java::com.droiuby.client.utils.intents.IntentIntegrator.new(me)
+            integrator.initiateScan
+          end
+          
+        async.perform {
+          Java::com.droiuby.client.core.utils.Utils.getLocalIpAddress(_current_activity)
+        }.done { |result|
+          V('#ip_address').text = "#{result}:4000"
+        }.start
+    end
   end
   
   def on_activity_result(request_code, result_code, intent)
